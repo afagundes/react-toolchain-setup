93007f51c082ddf3022a32591c434815c7e4512f
Archimedes Fagundes Junior <archimedes.junior@nava.com.br>
2023-01-31 08:56:55 -0300

    feat: add a functional component

---
+++ src/components/Greetings.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Greetings = ({name}) => {
+    return (
+        <div>
+            Hello from the inside of this component! I am {name}.
+        </div>
+    );
+}
+
+export default Greetings;
