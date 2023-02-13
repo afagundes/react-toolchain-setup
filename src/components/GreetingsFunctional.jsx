914888ea57596b0968f70a7ad8239bbec949bf3c
Archimedes Fagundes Junior <archimedes.junior@nava.com.br>
2023-02-13 11:48:34 -0300

    feat: add ProvidePlugin to resolve the 'React import'

--- src/components/GreetingsFunctional.jsx
+++ src/components/GreetingsFunctional.jsx
@@ -1,5 +1,3 @@
-import React from 'react';
-
 const GreetingsFunctional = ({name}) => {
     return (
         <div>
--- src/components/Image.jsx
+++ src/components/Image.jsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import logo from '../assets/images/webpack-logo.png';
 
 function Image() {
